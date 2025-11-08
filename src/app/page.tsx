9f4e102a85641c34970c88976caabb46438aced0
VampireXI <gapmanutsawin@gmail.com>
2025-11-08 14:00:56 +0700

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,10 +16,10 @@ export default function Home() {
         // const randomIndex = Math.floor(Math.random() * words.length);
         // const word = words[randomIndex];
 
-        const response = await fetch('/api/word');
-        const word = await response.json();
+        const response = await fetch('http://localhost:8000/api/word');
+        const data = await response.json();
 
-        setCurrentWord(word.data);
+        setCurrentWord(data);
         setSentence('');
         setScore(0);
         setFeedbackColor('text-gray-700');
